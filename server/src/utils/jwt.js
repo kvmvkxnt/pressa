e87592cc1c6a1bb9003fb8ab36aa87b67b382980
kvmvkxnt <kamron1007dada@gmail.com>
2022-08-03 22:50:40 +0500

    Some changes

--- server/src/utils/jwt.js
+++ server/src/utils/jwt.js
@@ -2,11 +2,11 @@ import { sign, verify } from 'jsonwebtoken';
 import { SECRET } from '../config/index.js';
 
 export default {
-  sign: (payload: any) => {
+  sign: (payload) => {
     const token = sign(payload, SECRET);
     return token;
   },
-  verify: (payload: string) => {
+  verify: (payload) => {
     const output = verify(payload, SECRET);
     return output;
   }
